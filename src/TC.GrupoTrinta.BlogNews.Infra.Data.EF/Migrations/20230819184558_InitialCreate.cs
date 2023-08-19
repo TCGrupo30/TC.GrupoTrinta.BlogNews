5e75086d44eaab4cdcdc0c9baa39556132a4fa8c
Genilson Maceno <genilson_de_maceno@hotmail.com>
2023-08-19 15:50:01 -0300

    Created migrations entity framework

---
+++ src/TC.GrupoTrinta.BlogNews.Infra.Data.EF/Migrations/20230819184558_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TC.GrupoTrinta.BlogNews.Infra.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false),
+                    Author = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    PublicationDate = table.Column<DateTime>(type: "Datetime", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "Datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "News");
+        }
+    }
+}
