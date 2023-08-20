10dad9adef46a0d2c9277a6c3b47848daecd8bab
Gabriel Prado Silva <gab.silva@reply.com>
2023-08-20 18:58:24 -0300

    Endpoints GET

---
+++ src/TC.GrupoTrinta.BlogNews.Application/UseCases/News/GetNews/GetAllNewsInput.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TC.GrupoTrinta.BlogNews.Application.UseCases.News.Common;
+
+namespace TC.GrupoTrinta.BlogNews.Application.UseCases.News.GetNews
+{
+    public class GetAllNewsInput : IRequest<List<NewsModelOutput>>
+    {
+    }
+}
--- src/TC.GrupoTrinta.BlogNews.Domain/SeedWork/IGenericRepository.cs
+++ src/TC.GrupoTrinta.BlogNews.Domain/SeedWork/IGenericRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace TC.GrupoTrinta.BlogNews.Domain.SeedWork;
+﻿using TC.GrupoTrinta.BlogNews.Domain.Entity;
+
+namespace TC.GrupoTrinta.BlogNews.Domain.SeedWork;
 public interface IGenericRepository<TAggregate> : IRepository where TAggregate : AggregateRoot
 {
     public Task Insert(TAggregate aggregate, CancellationToken cancellationToken);
+
+    public Task<TAggregate> GetById(Guid id, CancellationToken cancellationToken);
+
+    public Task<List<TAggregate>> GetAll(CancellationToken cancellationToken);
 }
