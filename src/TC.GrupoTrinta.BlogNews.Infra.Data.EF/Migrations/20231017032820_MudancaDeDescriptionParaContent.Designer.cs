a9b7990799054814518723cd24c1052862586d58
José Antonio <jose.antonio@7comm.com.br>
2023-10-17 00:29:06 -0300

    feat: mudança de coluna

--- src/TC.GrupoTrinta.BlogNews.Infra.Data.EF/Migrations/20231017032820_MudancaDeDescriptionParaContent.Designer.cs
+++ src/TC.GrupoTrinta.BlogNews.Infra.Data.EF/Migrations/20231017032820_MudancaDeDescriptionParaContent.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TC.GrupoTrinta.BlogNews.Infra.Data.EF;
 
@@ -11,9 +12,11 @@ using TC.GrupoTrinta.BlogNews.Infra.Data.EF;
 namespace TC.GrupoTrinta.BlogNews.Infra.Data.EF.Migrations
 {
     [DbContext(typeof(BlogNewsDbContext))]
-    partial class BlogNewsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017032820_MudancaDeDescriptionParaContent")]
+    partial class MudancaDeDescriptionParaContent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
